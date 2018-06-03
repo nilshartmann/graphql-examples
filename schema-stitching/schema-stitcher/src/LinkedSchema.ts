5e97db9bab18315ffa44e73c843518c47b90f816
Nils Hartmann <nils@nilshartmann.net>
2018-06-03 14:31:23 +0200

    meetup talk

--- schema-stitching/schema-stitcher/src/LinkedSchema.ts
+++ schema-stitching/schema-stitcher/src/LinkedSchema.ts
@@ -12,9 +12,13 @@ export const createLinkedSchema = (ratingSchema: GraphQLSchema) => ({
   linkedSchemaResolvers: {
     Beer: {
       ratings: {
+        // with the fragment we make sure, that the id field on Beer
+        // is always queried (even if not specified on original query),
+        // so that we can access it in our resolver
         fragment: `fragment BeerFragment on Beer { id }`,
         resolve(parent: any, args: any, context: any, info: any) {
           // parent is Beer
+          console.log(parent);
           const beerId = parent.id;
 
           return info.mergeInfo.delegateToSchema({
