b0c51952a95b5e0967527ded577ebe15c7e36335
Nils Hartmann <nils@nilshartmann.net>
2018-06-12 20:06:25 +0200

    Move Mutation to BeerRack

--- local-state/frontend/src/BeerRack.tsx
+++ local-state/frontend/src/BeerRack.tsx
@@ -1,21 +1,40 @@
 import * as React from "react";
+import { gql } from "apollo-boost";
+import { Mutation } from "react-apollo";
+import { SetCurrentBeerIdMutationResult, SetCurrentBeerIdMutationVariables } from "./__generated__/SetCurrentBeerIdMutation";
+
 import * as styles from "./BeerRack.scss";
 interface BeerRackProps {
   beerIds: { id: string }[];
-  onBeerSelected: (beerId: string) => void;
 }
 
-export default function BeerRack({ beerIds, onBeerSelected }: BeerRackProps) {
+const SET_CURRENT_BEER_ID_MUTATION = gql`
+  mutation SetCurrentBeerIdMutation($newBeerId: ID!) {
+    setCurrentBeerId(beerId: $newBeerId) @client
+  }
+`;
+
+export default function BeerRack({ beerIds }: BeerRackProps) {
+  // // Alternate:
+  // client.mutate<SetCurrentBeerIdMutationResult>({
+  //   mutation: SET_CURRENT_BEER_ID_MUTATION,
+  //   variables: { newBeerId: currentBeerId }
+  // });
+
   return (
-    <div className={styles.BeerRack}>
-      {beerIds.map(beerId => (
-        <div
-          key={beerId.id}
-          className={styles.Thumbnail}
-          style={{ backgroundImage: `url('/assets/beer/${beerId.id}.jpg')` }}
-          onClick={() => onBeerSelected(beerId.id)}
-        />
-      ))}
-    </div>
+    <Mutation<SetCurrentBeerIdMutationResult, SetCurrentBeerIdMutationVariables> mutation={SET_CURRENT_BEER_ID_MUTATION}>
+      {setCurrentBeerId => (
+        <div className={styles.BeerRack}>
+          {beerIds.map(beerId => (
+            <div
+              key={beerId.id}
+              className={styles.Thumbnail}
+              style={{ backgroundImage: `url('/assets/beer/${beerId.id}.jpg')` }}
+              onClick={() => setCurrentBeerId({ variables: { newBeerId: beerId.id } })}
+            />
+          ))}
+        </div>
+      )}
+    </Mutation>
   );
 }
--- local-state/frontend/src/BeerRatingApp.tsx
+++ local-state/frontend/src/BeerRatingApp.tsx
@@ -6,9 +6,9 @@ import * as styles from "./BeerRatingApp.scss";
 import Header from "./Header";
 import BeerPage from "./BeerPage";
 import { BeerRatingAppQueryResult } from "./__generated__/BeerRatingAppQuery";
-import { SetCurrentBeerIdMutationResult, SetCurrentBeerIdMutationVariables } from "./__generated__/SetCurrentBeerIdMutation";
 import BeerRack from "./BeerRack";
 import Footer from "./Footer";
+
 const BEER_RATING_APP_QUERY = gql`
   query BeerRatingAppQuery {
     beers {
@@ -19,19 +19,13 @@ const BEER_RATING_APP_QUERY = gql`
   }
 `;
 
-const SET_CURRENT_BEER_ID_MUTATION = gql`
-  mutation SetCurrentBeerIdMutation($newBeerId: ID!) {
-    setCurrentBeerId(beerId: $newBeerId) @client
-  }
-`;
-
 // TypeScript 2.9:
 const BeerRatingApp = () => (
   <div className={styles.BeerRatingApp}>
     <Header />
     <div className={styles.Main}>
       <Query<BeerRatingAppQueryResult> query={BEER_RATING_APP_QUERY}>
-        {({ loading, error, data }) => {
+        {({ loading, error, data, client }) => {
           if (loading) {
             return <h1>Loading...</h1>;
           }
@@ -40,20 +34,11 @@ const BeerRatingApp = () => (
             return <h1>Error...</h1>;
           }
 
-          const { currentBeerId } = data!;
-
           return (
-            <Mutation<SetCurrentBeerIdMutationResult, SetCurrentBeerIdMutationVariables> mutation={SET_CURRENT_BEER_ID_MUTATION}>
-              {setCurrentBeerId => (
-                <>
-                  <BeerRack
-                    beerIds={data!.beers}
-                    onBeerSelected={beerId => setCurrentBeerId({ variables: { newBeerId: beerId } })}
-                  />
-                  <BeerPage beerId={currentBeerId} />
-                </>
-              )}
-            </Mutation>
+            <>
+              <BeerRack beerIds={data!.beers} />
+              <BeerPage beerId={data!.currentBeerId} />
+            </>
           );
         }}
       </Query>
