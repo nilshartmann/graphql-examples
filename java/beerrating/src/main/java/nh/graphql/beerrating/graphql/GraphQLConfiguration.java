06626daf7168633785c777d0a235e0e72aef6b75
Nils Hartmann <nils@nilshartmann.net>
2018-06-30 21:04:22 +0200

    Add GraphiQL

---
+++ java/beerrating/src/main/java/nh/graphql/beerrating/graphql/GraphQLConfiguration.java
@@ -0,0 +1,24 @@
+package nh.graphql.beerrating.graphql;
+
+import graphql.schema.GraphQLSchema;
+import graphql.servlet.*;
+import org.springframework.boot.web.servlet.ServletRegistrationBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * @author Nils Hartmann (nils@nilshartmann.net)
+ */
+@Configuration
+public class GraphQLConfiguration {
+
+  @Bean
+  ServletRegistrationBean<SimpleGraphQLServlet> graphQLServletRegistrationBean(GraphQLSchema schema) {
+    final DefaultGraphQLSchemaProvider schemaProvider = new DefaultGraphQLSchemaProvider(schema);
+    SimpleGraphQLServlet.Builder builder = SimpleGraphQLServlet.builder(schemaProvider)
+        .withExecutionStrategyProvider(new DefaultExecutionStrategyProvider());
+
+    return new ServletRegistrationBean<>(builder.build(), "/graphql");
+  }
+
+}
