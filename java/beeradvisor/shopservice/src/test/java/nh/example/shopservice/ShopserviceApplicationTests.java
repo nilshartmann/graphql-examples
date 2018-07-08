d81e4c3d6040007faa6c3a4ece878f85abdec376
Nils Hartmann <nils@nilshartmann.net>
2018-07-08 10:38:08 +0200

    add shopservice

---
+++ java/beeradvisor/shopservice/src/test/java/nh/example/shopservice/ShopserviceApplicationTests.java
@@ -0,0 +1,16 @@
+package nh.example.shopservice;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ShopserviceApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
